c0a65e7e5a1925ab67f937dac314e2c1aca25e2c
Dani <danialtorehan8@gmail.com>
2026-01-30 09:11:48 +0500

    comments

---
+++ Practice1/comments.py
@@ -0,0 +1,19 @@
+#This is a note
+print("Welcome to Python!")
+#------------------------------------------------
+print("Learning is fun!") #This is a note
+#------------------------------------------------
+#print("Welcome to Python!")
+print("Have a great day!")
+#------------------------------------------------
+#This is a note
+#written across
+#multiple lines
+print("Keep coding!")
+#--------------------------------------------------
+"""
+This is a note
+written across
+multiple lines
+"""
+print("Success!")
