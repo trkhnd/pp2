39503fffd788e75e673b657bf3d659883ede9fce
Dani <danialtorehan8@gmail.com>
2026-02-07 09:45:01 +0500

    if, dict, set

---
+++ Practice2/dictionary.py
@@ -0,0 +1,54 @@
+#-------------------------------------------------
+thisdict = {
+  "brand": "Toyota",
+  "model": "Supra",
+  "year": 1998
+}
+
+#-------------------------------------------------
+thisdict = {
+  "brand": "Toyota",
+  "model": "Supra",
+  "year": 1998
+}
+x = thisdict["model"]
+
+#-------------------------------------------------
+thisdict = {
+  "brand": "Toyota",
+  "model": "Supra",
+  "year": 1998
+}
+thisdict["year"] = 2022
+
+#-------------------------------------------------
+thisdict = {
+  "brand": "Toyota",
+  "model": "Supra",
+  "year": 1998
+}
+thisdict["color"] = "black"
+print(thisdict)
+
+#-------------------------------------------------
+student1 = {
+  "name": "Alex",
+  "year": 2010
+}
+student2 = {
+  "name": "Mia",
+  "year": 2012
+}
+student3 = {
+  "name": "Leo",
+  "year": 2015
+}
+
+school = {
+  "student1": student1,
+  "student2": student2,
+  "student3": student3
+}
+
+print(school)
+
