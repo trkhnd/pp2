3c3410522f46c42974212fe6d5c3948006a0bd78
Dani <danialtorehan8@gmail.com>
2026-04-03 21:25:14 +0500

    essential

--- Practice-8/procedures.sql
+++ Practice-8/procedures.sql
@@ -1,7 +1,69 @@
+CREATE OR REPLACE PROCEDURE upsert_contact(
+    p_name TEXT,
+    p_surname TEXT,
+    p_phone TEXT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM contacts
+        WHERE name = p_name AND surname = p_surname
+    ) THEN
+        UPDATE contacts
+        SET phone = p_phone
+        WHERE name = p_name AND surname = p_surname;
+    ELSE
+        INSERT INTO contacts(name, surname, phone)
+        VALUES (p_name, p_surname, p_phone);
+    END IF;
+END;
+$$;
+
+CREATE OR REPLACE PROCEDURE insert_many_contacts(
+    p_names TEXT[],
+    p_surnames TEXT[],
+    p_phones TEXT[]
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    i INT;
+    n INT;
+BEGIN
+    n := array_length(p_names, 1);
+
+    IF n IS NULL OR n <> array_length(p_surnames, 1) OR n <> array_length(p_phones, 1) THEN
+        RAISE EXCEPTION 'Input arrays must have the same length';
+    END IF;
+
+    FOR i IN 1..n LOOP
+        IF p_phones[i] ~ '^[0-9]{11}$' THEN
+            IF EXISTS (
+                SELECT 1
+                FROM contacts
+                WHERE name = p_names[i] AND surname = p_surnames[i]
+            ) THEN
+                UPDATE contacts
+                SET phone = p_phones[i]
+                WHERE name = p_names[i] AND surname = p_surnames[i];
+            ELSE
+                INSERT INTO contacts(name, surname, phone)
+                VALUES (p_names[i], p_surnames[i], p_phones[i]);
+            END IF;
+        ELSE
+            INSERT INTO invalid_contacts(name, surname, phone, reason)
+            VALUES (p_names[i], p_surnames[i], p_phones[i], 'Invalid phone format');
+        END IF;
+    END LOOP;
+END;
+$$;
+
 CREATE OR REPLACE PROCEDURE delete_contact(
-    p_name VARCHAR DEFAULT NULL,
-    p_surname VARCHAR DEFAULT NULL,
-    p_phone VARCHAR DEFAULT NULL
+    p_name TEXT DEFAULT NULL,
+    p_surname TEXT DEFAULT NULL,
+    p_phone TEXT DEFAULT NULL
 )
 LANGUAGE plpgsql
 AS $$
